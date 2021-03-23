219bd7d2ccfb9da1049c7a69b34a14178e061a98
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-03-24 01:12:15 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("elyase","keceli");
+            Console.WriteLine(myDictionary.Length);
+
+            myDictionary.Add("engin", "töngel");
+            Console.WriteLine(myDictionary.Length);
+
+            
+
+        }
+    }
+}
